551181a579ad319983ba53d062906e2c5514eb0b
Mariya <quokka.mariya@gmail.com>
2024-12-10 15:06:37 +0300

    Задание про список

--- src/App.js
+++ src/App.js
@@ -1,47 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { useState } from 'react';
+
 export const App = () => {
-	const year = new Date().getFullYear(); // императивный стиль
-	// Далее декларативный стиль:
-	return /*#__PURE__*/ React.createElement(
-		'div',
-		{
-			className: 'App',
-		},
-		/*#__PURE__*/ React.createElement(
-			'header',
-			{
-				className: 'App-header',
-			},
-			/*#__PURE__*/ React.createElement('img', {
-				src: logo,
-				className: 'App-logo',
-				alt: 'logo',
-			}),
-			/*#__PURE__*/ React.createElement(
-				'p',
-				null,
-				'Edit ',
-				/*#__PURE__*/ React.createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			/*#__PURE__*/ React.createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			/*#__PURE__*/ React.createElement(
-				'p',
-				null,
-				'\u0421\u0435\u0439\u0447\u0430\u0441 ',
-				year,
-				' \u0433\u043E\u0434',
-			),
-		),
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+	const [isValueValid, setIsValueValid] = useState(true);
+
+	const formatDate = (date) => {
+		const day = String(date.getDate()).padStart(2, '0');
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const year = date.getFullYear();
+		const hours = String(date.getHours()).padStart(2, '0');
+		const minutes = String(date.getMinutes()).padStart(2, '0');
+		const seconds = String(date.getSeconds()).padStart(2, '0');
+
+		return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+	};
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение').trim();
+		if (promptValue.length < 3) {
+			setError('Введенное значение должно содержать минимум 3 символа');
+			setIsValueValid(false);
+		} else {
+			setValue(promptValue);
+			setError('');
+			setIsValueValid(true);
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (isValueValid && value.trim() !== '') {
+			const currentDate = formatDate(new Date());
+			setList((updatedList) => [
+				...updatedList,
+				{ id: Date.now(), value, currentDate },
+			]);
+			setValue('');
+			setError('');
+		}
+	};
+
+	return (
+		<>
+			<div className={styles.app}>
+				<h1 className={styles['page-heading']}>Ввод значения</h1>
+				<p className={styles['no-margin-text']}>
+					Текущее значение <code>value</code>: "
+					<output className={styles['current-value']}>{value}</output>"
+				</p>
+				{error !== '' && <div className={styles.error}>{error}</div>}
+				<div className={styles['buttons-container']}>
+					<button className={styles.button} onClick={onInputButtonClick}>
+						Ввести новое
+					</button>
+					<button
+						className={styles.button}
+						disabled={!isValueValid}
+						onClick={onAddButtonClick}
+					>
+						Добавить в список
+					</button>
+				</div>
+				<div className={styles['list-container']}>
+					<h2 className={styles['list-heading']}>Список:</h2>
+					{list.length === 0 ? (
+						<p className={styles['no-margin-text']}>
+							Нет добавленных элементов
+						</p>
+					) : (
+						<ul className={styles.list}>
+							{list.map((item) => (
+								<li key={item.id} className={styles['list-item']}>
+									{item.value}: {item.currentDate}
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</div>
+		</>
 	);
 };
