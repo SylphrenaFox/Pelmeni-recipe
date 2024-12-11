403a48bb98be18e6191acdb151f352aa0d61d202
Mariya <quokka.mariya@gmail.com>
2024-12-11 12:54:43 +0300

    Pelmeni recipe

--- src/App.js
+++ src/App.js
@@ -1,85 +1,72 @@
 import styles from './App.module.css';
+import data from './data.json';
 import { useState } from 'react';
 
 export const App = () => {
-	const [value, setValue] = useState('');
-	const [list, setList] = useState([]);
-	const [error, setError] = useState('');
-	const [isValueValid, setIsValueValid] = useState(true);
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-	const formatDate = (date) => {
-		const day = String(date.getDate()).padStart(2, '0');
-		const month = String(date.getMonth() + 1).padStart(2, '0');
-		const year = date.getFullYear();
-		const hours = String(date.getHours()).padStart(2, '0');
-		const minutes = String(date.getMinutes()).padStart(2, '0');
-		const seconds = String(date.getSeconds()).padStart(2, '0');
+	const isFirstStep = activeIndex === 0;
+	const isLastStep = activeIndex === steps.length - 1;
 
-		return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
-	};
-
-	const onInputButtonClick = () => {
-		const promptValue = prompt('Введите значение').trim();
-		if (promptValue.length < 3) {
-			setError('Введенное значение должно содержать минимум 3 символа');
-			setIsValueValid(false);
-		} else {
-			setValue(promptValue);
-			setError('');
-			setIsValueValid(true);
+	const onClickForward = () => {
+		if (activeIndex < steps.length - 1) {
+			setActiveIndex((updatedIndex) => updatedIndex + 1);
 		}
 	};
 
-	const onAddButtonClick = () => {
-		if (isValueValid && value.trim() !== '') {
-			const currentDate = formatDate(new Date());
-			setList((updatedList) => [
-				...updatedList,
-				{ id: Date.now(), value, currentDate },
-			]);
-			setValue('');
-			setError('');
+	const onClickBackward = () => {
+		if (activeIndex !== steps[0]) {
+			setActiveIndex((updatedIndex) => updatedIndex - 1);
 		}
+	};
+
+	const onClickBeginAgain = () => {
+		setActiveIndex(0);
 	};
 
 	return (
-		<>
-			<div className={styles.app}>
-				<h1 className={styles['page-heading']}>Ввод значения</h1>
-				<p className={styles['no-margin-text']}>
-					Текущее значение <code>value</code>: "
-					<output className={styles['current-value']}>{value}</output>"
-				</p>
-				{error !== '' && <div className={styles.error}>{error}</div>}
-				<div className={styles['buttons-container']}>
-					<button className={styles.button} onClick={onInputButtonClick}>
-						Ввести новое
-					</button>
-					<button
-						className={styles.button}
-						disabled={!isValueValid}
-						onClick={onAddButtonClick}
-					>
-						Добавить в список
-					</button>
-				</div>
-				<div className={styles['list-container']}>
-					<h2 className={styles['list-heading']}>Список:</h2>
-					{list.length === 0 ? (
-						<p className={styles['no-margin-text']}>
-							Нет добавленных элементов
-						</p>
-					) : (
-						<ul className={styles.list}>
-							{list.map((item) => (
-								<li key={item.id} className={styles['list-item']}>
-									{item.value}: {item.currentDate}
-								</li>
-							))}
-						</ul>
-					)}
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map(({ id }, index) => (
+							<li
+								key={id}
+								className={`${styles['steps-item']} ${
+									activeIndex === index ? styles.active : ''
+								} ${index < activeIndex ? styles.done : ''}`}
+							>
+								<button
+									className={styles['steps-item-button']}
+									onClick={() => setActiveIndex(index)}
+								>
+									{index + 1}
+								</button>
+							</li>
+						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							className={styles.button}
+							disabled={activeIndex === 0}
+							onClick={onClickBackward}
+						>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={isLastStep ? onClickBeginAgain : onClickForward}
+						>
+							{!isLastStep ? 'Далее' : 'Начать сначала'}
+						</button>
+					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
